cbb6eef8f34f7485c2f2b3a1b1e90bfff25bad97
kts102121 <ron@vrexlab.com>
2018-10-29 18:10:07 +0800

    initial commit

---
+++ src/main/java/org/kts1021/docker/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package org.kts1021.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
