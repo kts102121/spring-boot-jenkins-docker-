d4056e37b578205ddfdc34f33259a88e3c870193
kts102121 <ron@vrexlab.com>
2018-10-30 12:33:48 +0800

    logging

--- src/main/java/org/kts1021/docker/demo/DemoApplication.java
+++ src/main/java/org/kts1021/docker/demo/DemoApplication.java
@@ -1,12 +1,22 @@
 package org.kts1021.docker.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+    @Value("${JASYPT_ENCRYPTOR_PASSWORD}")
+    private String password;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(password);
     }
 }
--- src/main/java/org/kts1021/docker/demo/handler/GreetingHandler.java
+++ src/main/java/org/kts1021/docker/demo/handler/GreetingHandler.java
@@ -1,5 +1,7 @@
 package org.kts1021.docker.demo.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -7,9 +9,15 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.net.InetSocketAddress;
+import java.util.Optional;
+
 @Component
 public class GreetingHandler {
+    private static final Logger log = LoggerFactory.getLogger(GreetingHandler.class);
+
     public Mono<ServerResponse> hello(ServerRequest request) {
+
         return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(BodyInserters.fromObject("Hello World!"));
     }
 }
