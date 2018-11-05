cc9891fe5190519b821b7e129471fb0ac5b5d495
kts102121 <ron@vrexlab.com>
2018-11-05 16:32:39 +0900

    added debug logs

--- src/main/java/org/kts1021/docker/demo/handler/GreetingHandler.java
+++ src/main/java/org/kts1021/docker/demo/handler/GreetingHandler.java
@@ -12,9 +12,17 @@ import reactor.core.publisher.Mono;
 
 @Component
 public class GreetingHandler {
+    private final UserMapper userMapper;
+
+    public GreetingHandler(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
     private static final Logger log = LoggerFactory.getLogger(GreetingHandler.class);
 
     public Mono<ServerResponse> hello(ServerRequest request) {
+        log.info("User: {}", userMapper.findByUsername("ksh10211").getUsername());
+
         return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(BodyInserters.fromObject(new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new java.util.Date (System.currentTimeMillis())) + " / from: " + request.remoteAddress()));
     }
 }
