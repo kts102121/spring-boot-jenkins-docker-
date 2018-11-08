975faa69b139ea871e2fd1e8ad462b532edf3020
kts102121 <ron@vrexlab.com>
2018-11-08 17:08:12 +0900

    TooManyResultsException related bug fixes

--- src/main/java/org/kts1021/docker/demo/mapper/UserMapper.java
+++ src/main/java/org/kts1021/docker/demo/mapper/UserMapper.java
@@ -5,8 +5,10 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.kts1021.docker.demo.domain.User;
 
+import java.util.List;
+
 @Mapper
 public interface UserMapper {
     @Select("SELECT DISTINCT * FROM users WHERE username = #{username}")
-    User findByUsername(@Param("username") String username);
+    List<User> findByUsername(@Param("username") String username);
 }
